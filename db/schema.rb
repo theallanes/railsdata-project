4506ae699b65b7465ba9eaffa3efc1e277227e99
Thea <zllanes@academic.rrc.ca>
2025-03-01 22:27:18 -0600

    Created a Genre model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_02_042500) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_02_042651) do
+  create_table "genres", force: :cascade do |t|
+    t.string "genre_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "labels", force: :cascade do |t|
     t.string "label_name"
     t.datetime "created_at", null: false

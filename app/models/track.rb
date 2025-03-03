087891886e6250ac6c55d6070e07f192ee2a7403
Thea <zllanes@academic.rrc.ca>
2025-03-02 21:50:13 -0600

    Validation for track

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,4 +2,7 @@ class Track < ApplicationRecord
   belongs_to :artist
   belongs_to :genre
   belongs_to :album
+
+  validates :title, :release_year, presence: true
+  validates :release_year, numericality: { only_integer: true }
 end
